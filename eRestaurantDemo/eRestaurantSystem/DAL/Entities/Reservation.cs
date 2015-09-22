af7cb2002dfa0ddde752676f9ca5cfc80174bf30
SuperBigL <master_biglee@live.ca>
2015-09-22 15:23:01 -0600

    eRestaurant Demos Classes 1

--- eRestaurantDemo/eRestaurantSystem/DAL/Entities/Reservation.cs
+++ eRestaurantDemo/eRestaurantSystem/DAL/Entities/Reservation.cs
@@ -17,10 +17,13 @@ namespace eRestaurantSystem.Entities
         [StringLength(40)]
         public string CustomerName { get; set; }
         public DateTime ReservationDate { get; set; }
-
+        [Range(1,16, ErrorMessage="Party size is limited to 1 - 16")]
         public int NumberInParty { get; set; }
+        [StringLength(15)]
         public string ContactPhone { get; set; }
+        [Required, StringLength(1,MinimumLength=1)]
         public string ReservationStatus { get; set; }
+        [StringLength(1)]
         public string EventCode { get; set; }
 
         //Navigation properties
