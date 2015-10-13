dc4b1133d499b031725dbd078dcd7378a68d4680
SuperBigL <master_biglee@live.ca>
2015-10-13 17:03:35 -0600

    Changes 5

--- eRestaurantDemo/eRestaurantSystem/DAL/POCOs/ReservationDetail.cs
+++ eRestaurantDemo/eRestaurantSystem/DAL/POCOs/ReservationDetail.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations;
 
 namespace eRestaurantSystem.DAL.POCOs
 {
